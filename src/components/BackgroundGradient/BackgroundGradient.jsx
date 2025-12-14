38f147910abf3b6da2b97217d964ff97885cef5e
Nurrizzky <nurrizkyr90@gmail.com>
2025-12-14 21:19:47 +0700

    early-development: coba-mobile-error-fix2

--- src/components/BackgroundGradient/BackgroundGradient.jsx
+++ src/components/BackgroundGradient/BackgroundGradient.jsx
@@ -4,7 +4,7 @@ import gradientPoinBottom from '../../assets/images/gradientBottom.webp';
 export default function BackgroundGradient() {
    return (
       <>
-         <div className="fixed w-full -z-10 inset-0 box-border bg-[#111]">
+         <div className="fixed w-full h-full -z-10 inset-0 box-border bg-[#111]">
             {/* <img src={gradientPoinTop} alt="images" loading="eager" fetchPriority="high" className='opacity-[0.5] absolute w-full top-0 right-0 pointer-events-none' />
             <img src={gradientPoinBottom} alt="image" loading="eager" fetchPriority="high" className='opacity-[0.5] absolute w-full  bottom-0 left-0 pointer-events-none' /> */}
          </div>
