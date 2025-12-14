4fa6c838a38e9f4f8fdd79240b3343ab0eeb732e
Nurrizzky <nurrizkyr90@gmail.com>
2025-12-14 21:29:19 +0700

    early-development: coba-mobile-error-fix-4

--- src/layouts/SectionWrapper.jsx
+++ src/layouts/SectionWrapper.jsx
@@ -1,6 +1,6 @@
 export default function SectionWrapper({ children, id, style }) {
    return (
-      <section id={id} className={`min-h-dvh w-full px-10 ${style}`}>
+      <section id={id} className={`min-h-screen w-full px-10 ${style}`}>
          {children}
       </section>
    )
